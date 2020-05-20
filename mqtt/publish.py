2f1e0c44be08cd6e3ab0b4f2d15048f6bec0779a
xuebaocai <50941429+xuebaocai@users.noreply.github.com>
2020-05-20 15:08:45 +0800

    Update publish.py

--- mqtt/publish.py
+++ mqtt/publish.py
@@ -1,5 +1,6 @@
-# 2020.4.23
-#aicamera
+# 2020.5.20
+# aicamera
+
 import paho.mqtt.client as mqtt
 import json
 import numpy as np
@@ -10,42 +11,72 @@ from PIL import Image
 from imutils import opencv2matplotlib
 import cv2
 
-def pil_image_to_byte_array(image):
-    imgByteArr = io.BytesIO()
-    image.save(imgByteArr, "PNG")
-    return imgByteArr.getvalue()
+class Publish():
 
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-   
-def on_disconnect(client, userdata, rc):
-    #print("disconnect")
-    client.reconnect()
+    def __init__(self,host=None,topic=None,send_msg=False,send_img=False,img=None,msg=None):
+        '''
+        
+        :param host: 代理ip
+        :param topic: 主题
+        :param send_msg: 是否发送文字
+        :param send_img: 是否发送图片
+        :param img: 图片地址
+        :param msg: 文字
+        '''
+        self.send_img = send_img
+        self.send_msg = send_msg
+        self.host = host
+        self.topic = topic
+        self.msg = msg
+        self.img = img
 
-def main(alarm,image,device_id=1,serial_id=10,send=False):
-    client = mqtt.Client()
-    client.username_pw_set("aicamer_{}".format(device_id), "aicameraSecret$#")  # "admin", "password"
-    client.on_connect = on_connect
-    
-    client.will_set('zn/aicamera/{}/{}/alarm'.format(device_id,serial_id), 'Last will message', 0, False)
-    
-    client.on_disconnect = on_disconnect
-    client.reconnect_delay_set(1, 30)
+    def pil_image_to_byte_array(self,image):
+        imgByteArr = io.BytesIO()
+        image.save(imgByteArr, "PNG")
+        return imgByteArr.getvalue()
 
-    client.connect(host="183.129.253.180", port=1883, keepalive=60)
-    client.loop_start()
-    if send == True:
+
+    def on_connect(self,client, userdata, flags, rc):
+        print("Connected with result code " + str(rc))
+
+
+    def on_disconnect(self,client, userdata, rc):
+        # print("disconnect")
+        client.reconnect()
+
+
+    def send(self):
+        client = mqtt.Client()
+        #client.username_pw_set("aicamer_{}".format(device_id), "aicameraSecret$#")  # "admin", "password"
+        client.on_connect = self.on_connect
+
+        #client.will_set('zn/aicamera/{}/{}/alarm'.format(device_id, serial_id), 'Last will message', 0, False)
+
+        client.on_disconnect = self.on_disconnect
+        client.reconnect_delay_set(1, 30)
+
+        client.connect(host=self.host, port=1883, keepalive=60)
+        client.loop_start()
         # 发布MQTT信息
-        #alarm
-        client.publish(topic='zn/aicamera/{}/{}/alarm'.format(device_id,serial_id),payload=alarm,qos=2)
-        #image
-        np_array_RGB = opencv2matplotlib(image)  # Convert to RGB
-        image = Image.fromarray(np_array_RGB)  # PIL image
-        byte_array = pil_image_to_byte_array(image)
-        client.publish(topic='zn/aicamera/{}/{}/alarm'.format(device_id, serial_id), payload=byte_array, qos=2)
-        time.sleep(0.1)
+        if self.send_msg == True:
 
-    client.loop_stop()
+            # alarm
+            client.publish(topic=self.topic, payload=self.msg, qos=2)
+            time.sleep(0.1)
 
-if __name__=='__main__':
-    main(alarm='vvv',image=img,send=True)
+        if self.send_img == True:
+            # image
+            image = cv2.imread(self.img)
+            np_array_RGB = opencv2matplotlib(image)  # Convert to RGB
+            image = Image.fromarray(np_array_RGB)  # PIL image
+            byte_array = self.pil_image_to_byte_array(image)
+            client.publish(topic=self.topic, payload=byte_array, qos=2)
+            time.sleep(0.1)
+
+        client.loop_stop()
+
+
+if __name__ == '__main__':
+    pub = Publish(host='192.168.153.136',topic='zn/aicamera/image',send_msg=False,send_img=True,msg=None,img='C:/Users/windows10/Desktop/test_imgs/hat.jpg')
+    pub.send()
+
