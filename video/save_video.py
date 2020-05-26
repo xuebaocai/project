5a89314346cebb2173f3d5b3ebc45ae6546fa626
xuebaocai <50941429+xuebaocai@users.noreply.github.com>
2020-05-26 11:03:39 +0800

    Update save_video.py

--- video/save_video.py
+++ video/save_video.py
@@ -4,12 +4,10 @@ import time
 import avi_to_h264
 
 
-def main(ip='10.164.18.1'):
+def main(ip='10.164.18.1',save_time=1):
     '''
-
     :param ip: aicamera ip address
     :return: h264 video
-
     '''
     file_time = time.ctime()
     file_time = file_time.replace(' ', '_')
@@ -21,10 +19,13 @@ def main(ip='10.164.18.1'):
 
     fps = 25.0
 
-    frameSize = (640, 480)
+    frameSize = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 
     output = cv2.VideoWriter('{}.avi'.format(file_time), codec, fps, frameSize)
 
+    ctime = 0
+    ptime = time.time()
+    
     while (cap.isOpened()):
 
         ret, frame = cap.read()
@@ -33,16 +34,17 @@ def main(ip='10.164.18.1'):
 
         output.write(frame)
 
-        # stop the timer and convert to ms
-        stop_t = ((cv2.getTickCount() - start_t) / cv2.getTickFrequency()) * 1000
-        if stop_t >= 3000:
+        # stop 
+        ntime = time.time()
+        ctime = ntime - ptime
+        if ctime >= (save_time*2.9)*60:
             break
 
     cap.release()
     output.release()
     cv2.destroyAllWindows()
 
-    to_h264 = avi_to_h264(from_path='{}.avi'.format(file_time), to_path='{}.264'.format(file_time))
+    to_h264 = avi_to_h264.avi_to_H264(from_path='{}.avi'.format(file_time), to_path='{}.264'.format(file_time))
     to_h264.convert_byfile()
 
 if __name__ == "__main__":
