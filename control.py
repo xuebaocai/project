1ece7522c3c72b2901f707683f96469411663027
xuebaocai <50941429+xuebaocai@users.noreply.github.com>
2020-06-04 09:32:22 +0800

    Update control.py

--- control.py
+++ control.py
@@ -19,7 +19,6 @@ ser = serial.Serial(comport, baud)
 print(ser.isOpen())
 
 def parse_args():
-    """Parse input arguments."""
     desc = ('camer power')
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('--up',dest='power_up',action='store_true')
@@ -30,47 +29,47 @@ def parse_args():
 
 #power up
 def up():
-      up = bytearray.fromhex('01 10 00 E0 FF 00 55 AA')
-      ser.write(up)
-      time.sleep(0.1)
-      print('up done')
-      with open ('/home/mengjun/project/control/record.txt','w+') as f:
+    up = bytearray.fromhex('01 10 00 E0 FF 00 55 AA')
+    ser.write(up)
+    time.sleep(0.1)
+    print('up done')
+    with open ('/home/mengjun/project/control/record.txt','w+') as f:
         f.write('haha')
 
 #power down
 def down():
-      down = bytearray.fromhex('01 10 00 E0 00 00 55 AA')
-      ser.write(down)
-      time.sleep(0.1)       
-      print('down done')
+    down = bytearray.fromhex('01 10 00 E0 00 00 55 AA')
+    ser.write(down)
+    time.sleep(0.1)       
+    print('down done')
 
 #power read
 def read():
-        power = bytearray.fromhex('01 03 00 F0 00 01 55 AA')
-        ser.write(power)
-        time.sleep(0.1)
-        len_return_data = ser.inWaiting()
+    power = bytearray.fromhex('01 03 00 F0 00 01 55 AA')
+    ser.write(power)
+    time.sleep(0.1)
+    len_return_data = ser.inWaiting()
 
-        if len_return_data:
-                return_data = ser.read(len_return_data)
-                #print(return_data)
-                return_data = binascii.b2a_hex(return_data)
-                #print(return_data[6:10])
-                return_data = str(return_data[6:10])[2:6]
-                return_data = int(return_data,16)
-                print(return_data)
+    if len_return_data:
+        return_data = ser.read(len_return_data)
+        #print(return_data)
+        return_data = binascii.b2a_hex(return_data)
+        #print(return_data[6:10])
+        return_data = str(return_data[6:10])[2:6]
+        return_data = int(return_data,16)
+        print(return_data)
                 
 def main():
-        args = parse_args()
+    args = parse_args()
 
-        if args.power_up:
-                up()
+    if args.power_up:
+            up()
 
-        if args.power_down:
-                down()
+    if args.power_down:
+            down()
 
-        if args.power_read:
-                read()
+    if args.power_read:
+            read()
        
 if __name__ == '__main__':
     #main()
