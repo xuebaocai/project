2dfebd3fef21dd86357d4a42538ea28ce0adc7b8
xuebaocai <50941429+xuebaocai@users.noreply.github.com>
2020-06-19 16:09:34 +0800

    Update and rename mutl_cam to mutl_cam.py

--- atlas200/mutl_cam.py
+++ atlas200/mutl_cam.py
@@ -1,3 +1,6 @@
+#2020.6.19 by mengjun V1.0
+# opencv mutl ipcamera 
+
 import cv2
 import sys
 import time
@@ -6,8 +9,6 @@ import multiprocessing as mp
 import yolov3_tiny
 from settings import cam_addrs, img_shape
 from argparse import ArgumentParser
-
-
 
 def push_image(raw_q, cam_addr):
     cap = cv2.VideoCapture(cam_addr, cv2.CAP_FFMPEG)
@@ -23,7 +24,6 @@ def push_image(raw_q, cam_addr):
         else:
             # wait for stremaing
             time.sleep(0.01)
-
 
 def predict(raw_q, pred_q):
     while True:
@@ -42,7 +42,6 @@ def pop_image(pred_q, window_name, img_shape):
         cv2.imshow(window_name, frame)
         cv2.waitKey(1)
 
-
 def display(cam_addrs, window_names, img_shape=(300, 300)):
     raw_queues = [mp.Queue(maxsize=2) for _ in cam_addrs]
     pred_queues = [mp.Queue(maxsize=4) for _ in cam_addrs]
@@ -59,13 +58,11 @@ def display(cam_addrs, window_names, img_shape=(300, 300)):
 
 if __name__ == '__main__':
     mp.set_start_method(method='spawn')
-
     parser = ArgumentParser()
     parser.add_argument('--num_cameras', '-n', type=int,
                         help='number of cameras to process')
     args = parser.parse_args()
     args.num_cameras = len(cam_addrs) if args.num_cameras is None else args.num_cameras
     print(args.num_cameras)
-    
     
     display(cam_addrs[:args.num_cameras], ['camera' for _ in cam_addrs], img_shape)
