ad2759062f3d9309888aec85773b1ca5107a55a2
xuebaocai <50941429+xuebaocai@users.noreply.github.com>
2020-06-23 09:43:34 +0800

    Create setting.py

---
+++ atlas200/setting.py
@@ -0,0 +1,8 @@
+cam_addrs = [
+  #  "rtsp://admin:182333@10.164.18.1:554",
+"mobilessd_test.mp4",
+"/home/HwHiAiUser/person.mp4",
+]
+model_path = './model/deploy.om'
+CLASSES = ("background","person","hat","digger","truck")
+img_shape = (300,300)
