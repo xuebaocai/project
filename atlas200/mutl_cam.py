a888c3a1efc25411982b60ba1dc8db10db86092f
xuebaocai <50941429+xuebaocai@users.noreply.github.com>
2020-06-23 09:42:34 +0800

    Update mutl_cam.py

--- atlas200/mutl_cam.py
+++ atlas200/mutl_cam.py
@@ -1,14 +1,19 @@
-#2020.6.19 by mengjun V1.0
+#2020.6.23 by mengjun V1.1
 # opencv mutl ipcamera 
+
 
 import cv2
 import sys
 import time
 import numpy as np
 import multiprocessing as mp
-from settings import cam_addrs, img_shape
+from settings import cam_addrs, img_shape,model_path,CLASSES
 from argparse import ArgumentParser
-import yolov3_tiny
+import hiai
+from atlasutil import ai
+
+model_path = model_path
+CLASSES = CLASSES
 
 
 def push_image(raw_q, cam_addr):
@@ -28,28 +33,32 @@ def push_image(raw_q, cam_addr):
 
 
 def predict(raw_q, pred_q):
-    model = yolov3_tiny.general_yolov3()
+    my_graph = ai.Graph(model_path)
     while True:
-        
-         raw_img = raw_q.get()
-         result = model.predict(raw_img)
+        raw_img = raw_q.get()
+        raw_img = cv2.resize(raw_img,img_shape)
+        result_list = my_graph.Inference(raw_img)
+        if not result_list:
+            print("get no result")
+            return
+        h, w = raw_img.shape[0], raw_img.shape[1]
+        solution = (h, w)
+        detection_result_list = ai.SSDPostProcess(result_list[0], solution, 0.5, CLASSES)
+        for result in detection_result_list:
+            pred_q.put(result.result_text)
 
-         label = model.vis_res(raw_img, result)
-         pred_q.put(label)
 
 
 def pop_result(pred_q,ip):
-
     while True:
-        result = pred_q.get()
-        print (result,ip)
-
-
+        if not pred_q.empty():
+            result = pred_q.get()
+            print (result,ip)
 
 
 def display(cam_addrs, ips):
-    raw_queues = [mp.Queue(maxsize=2) for _ in cam_addrs]
-    pred_queues = [mp.Queue(maxsize=4) for _ in cam_addrs]
+    raw_queues = [mp.Queue(maxsize=16) for _ in cam_addrs]
+    pred_queues = [mp.Queue(maxsize=32) for _ in cam_addrs]
     processes = []
 
     for raw_q, pred_q, cam_addr, ip in zip(raw_queues, pred_queues, cam_addrs, ips):
@@ -61,6 +70,7 @@ def display(cam_addrs, ips):
     [process.start() for process in processes]
     [process.join() for process in processes]
 
+
 if __name__ == '__main__':
     mp.set_start_method(method='spawn')
 
@@ -70,5 +80,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     args.num_cameras = len(cam_addrs) if args.num_cameras is None else args.num_cameras
     print(args.num_cameras)
-
     display(cam_addrs[:args.num_cameras], [ip for ip in cam_addrs])
+
+
+
