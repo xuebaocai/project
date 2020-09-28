f01ddc51ce32ac983d9842cc67fd9cc71c95b081
xuebaocai <50941429+xuebaocai@users.noreply.github.com>
2020-09-28 15:04:31 +0800

    Update timer_job.py

--- timer_job.py
+++ timer_job.py
@@ -3,11 +3,16 @@ from config import Host,Latit,Longit,Distance,Power
 from geopy.distance import geodesic
 from control.camera_power import gps_read,power_read
 from mqtt.publish import Publish
-from config import Host
 import threading
+import json
 
+json_path = '/home/mengjun/xianlu/tensorrt_demos/utils/config.json'
+with open(json_path,'r') as f:
+   config = json.load(f)
 
-pub = Publish(host=Host)
+print(config)
+
+pub = Publish(host=config['Mqtt_pub'])
 
 def func(latit,longit,power):
     pub.send_msg(topic='/zn/aicamera/gps', msg='N-{},E-{}'.format(latit, longit))
@@ -15,29 +20,28 @@ def func(latit,longit,power):
 
 def dojob(ti,latit,longit,power):
     scheduler = BlockingScheduler()
-    # 添加任务,时间间隔ti MIN
+    # 定时任务
     scheduler.add_job(func, 'interval', minutes=ti, id='job1',args=(latit,longit,power))
     scheduler.start()
 
 while True:
     # GPS
     latit, longit = gps_read()
-    # 判断是否信号
+    # 是否有信号
     if latit == '' or latit == 0:
-       pub.send_msg(topic='/zn/aicamera/gps',msg='Weak GPS signal')
-    # 如果有判断移位距离
+       pub.send_msg(topic='/zn/aicamera/gps',msg='Weak GPS signal')  
     else:
         print(latit)
         latit = int(str(latit)[:2])+int(str(latit)[2:])/60
         longit = int(str(longit)[:3])+int(str(latit)[3:])/60
-        distance = geodesic((latit, longit), (Latit, Longit)).m
-        if distance > Distance:
+        distance = geodesic((latit, longit), (config['Latit'], config['Longit'])).m
+        if distance > config['Distance']:
             pub.send_msg(topic='/zn/aicamera/gps', msg='Displacement occurs')
 
     # power
     power = power_read()
     #print(power)
-    if type(power)==int and power < int(Power):
+    if type(power)==int and power < config['Power']:
        pub.send_msg(topic='/zn/aicamera/power', msg='Low power')
 
     timer = threading.Thread(target=dojob, args=(5 * 60, latit, longit, power,))
