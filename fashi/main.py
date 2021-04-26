5423a1053fd3e18749897b281b79654cbc7c8bfe
xuebaocai <50941429+xuebaocai@users.noreply.github.com>
2021-04-26 14:19:18 +0800

    Update main.py

--- fashi/main.py
+++ fashi/main.py
@@ -1,8 +1,7 @@
-"""
-date 2020.12.24 by mengjun
-阀室一至两个摄像头识别
-待改进：
-使用cv软解码，未找到nvr多路硬解码。
+"""trt_ssd.py
+
+This script demonstrates how to do real-time object detection with
+TensorRT optimized Single-Shot Multibox Detector (SSD) engine.
 """
 
 
@@ -18,12 +17,13 @@ from utils.ssd import TrtSSD
 from utils.camera import add_camera_args, Camera
 from utils.display import open_window, set_display, show_fps
 from utils.visualization import BBoxVisualization
-from utils.data_sys import DataSynchronization
+from utils.data_sys import Data_sys
+from utils.publish import Publish
 
 WINDOW_NAME = 'TrtSsdDemo'
 INPUT_HW = (300, 300)
 SUPPORTED_MODELS = [
-    'ssd_mobilenet_v1_digger',
+    'ssd_mobilenet_v1_coco',
 ]
 
 
@@ -35,48 +35,93 @@ def parse_args():
     parser = argparse.ArgumentParser(description=desc)
     parser = add_camera_args(parser)
     parser.add_argument('-m', '--model', type=str,
-                        default='ssd_mobilenet_v1_digger',
+                        default='ssd_mobilenet_v1_coco',
                         choices=SUPPORTED_MODELS)
     args = parser.parse_args()
     return args
 
 def main(config):
     args = parse_args()
+    data_sys = Data_sys()
+    pub = Publish(host=config['Mqtt_pub'])
 
     cls_dict = get_cls_dict(args.model.split('_')[-1])
     trt_ssd = TrtSSD(args.model, INPUT_HW)
-    while True:
-      img_list = []
-      if len(config['Channel']) == 1:
-        print('1')
-        cap = cv2.VideoCapture('rtsp://admin:admin12345@{}:554/Streaming/Channels/{}01'.format(config['Camera_ip'],config['Channel'][0]), cv2.CAP_FFMPEG)
+
+    img_list = []
+
+    # video
+    cap = cv2.VideoCapture('xiasha.avi')
+    i = 0
+    while cap.isOpened():
+        frame, img = cap.read()
+        if img is not None:
+
+            img_list.append([img])
+            result = trt_ssd.detect(img_list[0], conf_th=0.3)
+            # print(result)
+            data_sys.dataSynchronization(result, img_list, args.model, ['boundary_intrude', None], config['Zone'],
+                                         config['Channel'][0], config['device_id'], pub, config['Polygon'])
+            img_list = []
+            i = i + 1
+            print(i)
+        else:
+            msg = json.dumps({
+                "nvr_id": config['Zone'],
+                "device_id": config['device_id'],
+                "channel_id": config['Channel'][0]})
+            pub.send_msg(topic= "zs/ai_spxwfx/rtsp/" + config['Zone'] + "/" + config['Channel'][0], msg=msg, Zone=config['Zone'], device_id=config['device_id'])
+            
+            
+    '''
+    #img
+    img = cv2.imread('zhannei_9.jpg')
+    img_list.append([img])
+    result = trt_ssd.detect(img_list[0], conf_th=0.3)
+    #print(result)
+    DataSynchronization(result,img_list,args.model,['boundary_intrude',None],config['Zone'],config['Channel'][0],config['Mqtt_pub'],config['Polygon'],history_data,start_time)
+    
+    #rstp
+    if len(config['Channel']) == 1:
+      print('1')
+      #cap = cv2.VideoCapture('rtsp://admin:admin12345@{}:554/Streaming/Channels/{}01'.format(config['Camera_ip'],config['Channel'][0]))
+      cap = cv2.VideoCapture('xiasha.avi')
+      while cap.isOpened():
         frame,img = cap.read()
         if img is not None:
+          print(img.shape)
           img_list.append([img])
+          result = trt_ssd.detect(img_list[0], conf_th=0.3)
+          if len(result[0][0]) != 0 :
+            DataSynchronization(result,img_list,args.model,['boundary_intrude',None],config['Zone'],config['Channel'],config['Mqtt_pub'],config['Polygon'])
         else:
           continue
         
         
-      if len(config['Channel']) == 2:
-        print('2')
-        cap1 = cv2.VideoCapture('rtsp://admin:admin12345@{}:554/Streaming/Channels/{}01'.format(config['Camera_ip'],config['Channel'][0]), cv2.CAP_FFMPEG)
-        cap2 = cv2.VideoCapture('rtsp://admin:admin12345@{}:554/Streaming/Channels/{}01'.format(config['Camera_ip'],config['Channel'][1]), cv2.CAP_FFMPEG)
+    if len(config['Channel']) == 2:
+      print('2')
+      cap1 = cv2.VideoCapture('rtsp://admin:admin12345@{}:554/Streaming/Channels/{}01'.format(config['Camera_ip'],config['Channel'][0]))
+      cap2 = cv2.VideoCapture('rtsp://admin:admin12345@{}:554/Streaming/Channels/{}01'.format(config['Camera_ip'],config['Channel'][1]))
+      while cap1.isOpened() and cap2.isOpened():
         frame,img1 = cap1.read()
         frame,img2 = cap2.read()
         if img1 is not None and img2 is not None:
+          print(img1.shape,img2.shape)
           img_list.append([img1,img2])
+          result = trt_ssd.detect(img_list[0], conf_th=0.3)
+          print(result)
+          if len(result[0][0]) !=0 or len(result[1][0]) != 0:
+            DataSynchronization(result,img_list,args.model,['boundary_intrude',None],config['Camera_ip'],config['Channel'],config['Mqtt_pub'],config['Polygon'])
         else:
           continue
-      result = trt_ssd.detect(img_list[0], conf_th=0.5)
-      DataSynchronization(result,img_list,args.model,'boundary_intrude',None,config['Camera_ip'],config['Channel'],config['Mqtt_pub'],config['Polygon'])
-      
+    
     if len(config['Channel']) == 1:
       cap.release()
     else:
       cap1.release()
       cap2.release()
     cv2.destroyAllWindows()
-
+    '''
 
 if __name__ == '__main__':
     pwd = os.getcwd()
@@ -84,3 +129,4 @@ if __name__ == '__main__':
     with open(json_path,'r') as f:
        config = json.load(f)
     main(config)
+
